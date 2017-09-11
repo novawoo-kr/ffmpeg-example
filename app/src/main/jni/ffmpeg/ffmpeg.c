7d54890a8863eae05b94530a75a49b60176bd432
Nova Woo <novawoo.kr@gmail.com>
2017-09-12 05:56:04 +0900

    Change mid pred function for arm

--- app/src/main/jni/ffmpeg/ffmpeg.c
+++ app/src/main/jni/ffmpeg/ffmpeg.c
@@ -4804,16 +4804,21 @@ int main(int argc, char **argv)
 
 static inline av_const int mid_pred(int a, int b, int c)
 {
-    if(a>b){
-        if(c>b){
-            if(c>a) b=a;
-            else    b=c;
-        }
-    }else{
-        if(b>c){
-            if(c>a) b=c;
-            else    b=a;
-        }
-    }
-    return b;
+    int m;
+    __asm__ (
+        "mov   %0, %2  \n\t"
+        "cmp   %1, %2  \n\t"
+        "itt   gt      \n\t"
+        "movgt %0, %1  \n\t"
+        "movgt %1, %2  \n\t"
+        "cmp   %1, %3  \n\t"
+        "it    le      \n\t"
+        "movle %1, %3  \n\t"
+        "cmp   %0, %1  \n\t"
+        "it    gt      \n\t"
+        "movgt %0, %1  \n\t"
+        : "=&r"(m), "+r"(a)
+        : "r"(b), "r"(c)
+        : "cc");
+    return m;
 }
